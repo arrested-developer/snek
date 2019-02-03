0771ea5af7f4859480b9f0f03182116cd911b0c8
Michael Watts <hello@michaelwatts.co>
2019-02-03 21:03:41 +0000

    update fonts

--- src/Board.js
+++ src/Board.js
@@ -28,8 +28,8 @@ const Egg = styled(Snake)`
 const Board = props => {
   return (
     <Grid gridSize={props.gridSize}>
-      {props.snake.map(s => (
-        <Snake posX={s[0]} posY={s[1]} />
+      {props.snake.map((s, i) => (
+        <Snake key={i} posX={s[0]} posY={s[1]} />
       ))}
       {props.egg && <Egg posX={props.egg[0]} posY={props.egg[1]} />}
     </Grid>
