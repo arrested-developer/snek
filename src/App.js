525dfaaab3f5c67b22380bee4dde922d71a8ea8b
Michael Watts <hello@michaelwatts.co>
2019-02-03 09:45:56 +0000

    wrap snake when it leaves board

--- src/App.js
+++ src/App.js
@@ -13,36 +13,36 @@ class App extends Component {
   constructor() {
     super();
     this.state = { snake: [[1, 2], [2, 2], [3, 2]] };
+    this.gridSize = 20;
+    this.dir = 'right';
   }
   componentDidMount() {
     this.interval = setInterval(this.update, 500);
   }
   update = () => {
-    this.moveSnake('down');
+    this.moveSnake(this.dir);
   };
   moveSnake = dir => {
-    let changeX = 0;
-    let changeY = 0;
-    if (dir === 'right') {
-      changeX = 1;
-    } else if (dir === 'left') {
-      changeX = -1;
-    } else if (dir === 'down') {
-      changeY = 1;
-    } else if (dir === 'up') {
-      changeY = -1;
-    }
     this.setState(prev => {
-      const [lastX, lastY] = prev.snake[prev.snake.length - 1];
-      const newPos = [lastX + changeX, lastY + changeY];
-      return { snake: prev.snake.concat([newPos]) };
+      const [x, y] = prev.snake[prev.snake.length - 1];
+      let nextPos;
+      if (dir === 'right') {
+        nextPos = [x === this.gridSize ? 1 : x + 1, y];
+      } else if (dir === 'left') {
+        nextPos = [x === 1 ? this.gridSize : x - 1, y];
+      } else if (dir === 'down') {
+        nextPos = [x, y === this.gridSize ? 1 : y + 1];
+      } else if (dir === 'up') {
+        nextPos = [x, y === 1 ? this.gridSize : y - 1];
+      }
+      return { snake: prev.snake.concat([nextPos]) };
     });
   };
   render() {
     return (
       <div className="App">
         <H1>Snek</H1>
-        <Board snake={this.state.snake} />
+        <Board snake={this.state.snake} gridSize={this.gridSize} />
       </div>
     );
   }
